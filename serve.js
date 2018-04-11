a07a029fa6b0447e2412efc2a92708a6dd2c3e26
mrcreel <michaelrcreel@gmail.com>
2018-04-11 07:57:43 -0500

    Update package.json

---
+++ serve.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const GraphqlHTTP = require('express-graphql')
+
+const app = express()
+const schema = require('./schema')
+
+const APP_PORT = 4000
+
+app.use('/graphql', GraphqlHTTP(() => ({
+  schema: schema,
+  graphiql: true,
+  })
+))
+app.listen(APP_PORT)
+
+console.log(`Started on http://localhost:${APP_PORT}/`)
